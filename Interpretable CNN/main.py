728afd71b5af4cecf90a3e54d11075291a35d2fe
WEI Jingren <69457975+WEIwjr@users.noreply.github.com>
2020-10-10 11:33:06 -0700

    Add files via upload

--- Interpretable CNN/main.py
+++ Interpretable CNN/main.py
@@ -1,9 +1,11 @@
 import InterpretableCNN
 import data
+import sys
 
 def main():
 	# read data
-    train_images, train_labels, test_images, test_labels, class_list = data.import_data("0-9")
+    dataset = sys.argv[1]
+    train_images, train_labels, test_images, test_labels, class_list = data.import_data(dataset, "0-9")
     print('Training image size:', train_images.shape)
     print('Testing_image size:', test_images.shape)
     myCNN=InterpretableCNN.InterpretableCNN(train_images, train_labels, test_images, test_labels, class_list)
